f392d6ec114735685320fe7fb548d7d4305b7557
abhinav0451 <154614190+abhinav0451@users.noreply.github.com>
2024-01-15 08:54:15 +0000

    removed unnecessary code

--- dotagent_website/dotagent_website/components/all_agents.py
+++ dotagent_website/dotagent_website/components/all_agents.py
@@ -4,34 +4,31 @@ import nextpy as xt
 def all_agents_img():
     return xt.box(
         xt.box(
-            xt.box(
-                xt.image(src="/AllAgentsCard.png", class_name="w-52 h-28"),
-            ),
-            xt.box(
-                xt.text(
-                    "Mobile Legends: Bang Bang",
-                    class_name="lg:text-2xl text-[1.3rem] font-[DMSans] font-medium leading-6 text-white",
-                ),
-                xt.text(
-                    "Play with the world!....",
-                    class_name="text-xs py-2 text-white font-[DMSans]",
-                ),
-                xt.box(
-                    xt.box(
-                        "Get",
-                        class_name="flex items-center border-0 rounded-3xl max-h-[1.38rem] tracking-normal font-[DMSans] px-6 py-4 bg-[#8e55ea] text-white lg:text-lg text-base",
-                    ),
-                    xt.image(
-                        src="/ShareButton.png",
-                        class_name="w-10 h-9",
-                    ),
-                    class_name="flex justify-between gap-20 items-center mt-4 md:mt-1",
-                ),
-                class_name="flex flex-col ",
-            ),
-            class_name="flex gap-4",
+            xt.image(src="/AllAgentsCard.png", class_name="w-52 h-28"),
         ),
-        class_name="md:p-5 p-3 bg-[#1b1e1f]  my-4 rounded-3xl",
+        xt.box(
+            xt.text(
+                "Mobile Legends: Bang Bang",
+                class_name="lg:text-2xl text-[1.3rem] font-[DMSans] font-medium leading-6 text-white",
+            ),
+            xt.text(
+                "Play with the world!....",
+                class_name="text-xs py-2 text-white font-[DMSans]",
+            ),
+            xt.box(
+                xt.box(
+                    "Get",
+                    class_name="flex items-center border-0 rounded-3xl max-h-[1.38rem] tracking-normal font-[DMSans] px-6 py-4 bg-[#8e55ea] text-white lg:text-lg text-base",
+                ),
+                xt.image(
+                    src="/ShareButton.png",
+                    class_name="w-10 h-9",
+                ),
+                class_name="flex justify-between items-center mt-4 md:mt-1",
+            ),
+            class_name="flex justify-center lg:w-3/4 ml-3 w-3/4 flex-col",
+        ),
+        class_name="flex gap-4 md:p-5 p-3 bg-[#1b1e1f]  my-4 rounded-3xl",
     )
 
 
